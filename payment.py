b0e00457e1872512643ca50c5a1c36392ef4049a
GaryChenCY <jack0977234207123.com>
2024-11-08 17:47:27 +0800

    1.1

--- payment.py
+++ payment.py
@@ -11,21 +11,21 @@ stripe.api_key = "sk_test_51QBrlCByxTEIQfBXCEuz9gYnoZD53sOZR80clSPblmSW3MbtmEsM5
 def front():
     return render_template("front.html")
 
-# 創建支付路由
+
 @app.route('/create-payment-intent', methods=['POST'])
 def create_payment_intent():
     try:
-        # 從请求中獲取支付金額和支付方法 ID
+        # 從請求中獲取支付金額和支付方法 ID
         data = request.json
         amount = int(data['amount']) * 100  # Stripe 接受的金額單位為分
 
-        # 創建支付意圖 (Payment Intent)
+        # 創建支付 (Payment Intent)
         payment_intent = stripe.PaymentIntent.create(
             amount=amount,   # 支付金額，單位為分（例如 TWD 100 = 10000 分
             currency='twd',
             payment_method=data['payment_method_id'],  # 客户端傳入的支付方法 ID
             confirmation_method='manual',  # 手動確認支付
-            confirm=True,   # 自動確認支付意圖
+            confirm=True,   # 自動確認支付
             return_url="http://localhost:4242/success"  # 客户完成支付後的跳轉連接
         )
 
@@ -39,7 +39,7 @@ def create_payment_intent():
 # 成功頁面
 @app.route('/success', methods=['GET'])
 def success():
-    return "<h1>支付成功！謝謝您的購買。</h1>"
+    return "<h1>付款成功！謝謝您的購買。</h1>"
 
 if __name__ == '__main__':
     app.run(port=4242)
